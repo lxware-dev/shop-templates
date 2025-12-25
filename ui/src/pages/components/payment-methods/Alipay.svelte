35891852dd634933009b5c4a9bf0ec6ff12d5b91
Ryan Wang <i@ryanc.cc>
2025-12-25 21:42:51 +0800

    Update Alipay.svelte

--- ui/src/pages/components/payment-methods/Alipay.svelte
+++ ui/src/pages/components/payment-methods/Alipay.svelte
@@ -30,7 +30,7 @@
         <iframe
           title="支付宝支付"
           srcdoc={alipay.pageRedirectionData}
-          style="width: 200px; height: 200px;"
+          style="width: 200px; height: 205px;"
         ></iframe>
       </div>
       <div class="shop-qrcode-info">
